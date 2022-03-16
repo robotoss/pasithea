7c32a27e182993aabc50f9ac9eada969353f52e4
Konstantin Yeftifeyev <zoxo@outlook.com>
2022-03-16 21:32:44 +0200

    Fix path

--- src/help.rs
+++ src/help.rs
@@ -5,11 +5,11 @@ use clap::Parser;
 #[clap(author, version, about, long_about = None)]
 pub struct Cli {
     /// Path to the folder with voice recognition models
-    #[clap(short, long, default_value = "/models")]
+    #[clap(short, long, default_value = "models")]
     pub model_dir: String,
 
     /// Path to the audio file
-    #[clap(short, long, default_value = "/audio")]
+    #[clap(short, long, default_value = "audio/2830-3980-0043.wav")]
     pub audio_file_path: String,
 
 
